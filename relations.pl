9c8157e074508b5eb8a45690379021f9b89e084c
jannick <jannickboese@msn.com>
2014-04-24 15:56:37 +0200

    The war has been won

--- relations.pl
+++ relations.pl
@@ -12,20 +12,8 @@ query_redundant(part_of, A, B) :-
 query(R, A, B) :- setof(_, query_redundant(R,A,B), _).
 
 overlap(C, D, X) :- 
-	class(C), 
-	class(D), 
-	D @< C, 
-	overlap(D, C, X).
-
-overlap(C, D, X) :- 
 	query(is_a, X, C), 
 	query(is_a, X, D).
-
-disjoint(C, D) :- 
-	class(C), 
-	class(D), 
-	D @> C, 
-	disjoint(D, C).
 
 disjoint(C, D) :- 
 	class(C), 
