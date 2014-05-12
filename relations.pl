f8a87a415aa49ab3d0858db0ba6fd2127059f96d
jannick <jannickboese@msn.com>
2014-05-12 16:23:48 +0200

    updated relations db

--- relations.pl
+++ relations.pl
@@ -1,29 +1,44 @@
 ['~/Ontological/relationsdb'].
 
-transitivity(R,A,B):-relation(R,A,B).
-transitivity(R,A,C):-relation(is_a,B,C),transitivity(R,A,B).
-transitivity(R,A,C):-R \= is_a,relation(is_a,A,B),transitivity(R,B,C).
-transitivity(R,A,C):-relation(R,A,B),transitivity(R,B,C).
 
-query_redundant(is_a, A, A):-class(A).
-query_redundant(R, A, B) :- transitivity(R,A,B).
-query_redundant(part_of, A, B) :- 
-	query_redundant(part_for, A, B), 
-	query_redundant(has_part, B, A). 
+transitivity(R,A,B):-ae_relation(R,A,B).
+transitivity(R,A,C):-ae_relation(is_a,B,C),transitivity(R,A,B).
+transitivity(R,A,C):-R \= is_a,ae_relation(is_a,A,B),transitivity(R,B,C).
+transitivity(R,A,C):-ae_relation(R,A,B),transitivity(R,B,C).
 
-query(R, A, B) :- setof(_, query_redundant(R,A,B), _).
 
-is_reflexive(R,A):-R \= is_a, relation(R,A,A).
-is_symmetric(R,A,B):- query(R,A,B), query(R,B,A),\+ (A = B, R = is_a).
+ae_redundant(R, A, B) :- transitivity(R,A,B).
+ae_redundant(part_of, A, B) :- 
+	ae_redundant(part_for, A, B), 
+	ae_redundant(has_part, B, A). 
+ae_redundant(is_a, A, A):-class(A).
 
-ancestor(P, Q):-query(is_a, P, Q).
+ae(R, A, B) :- setof(_, ae_redundant(R,A,B), _).
 
-common_desendent(C, A, B) :- 
+is_reflexive(R,A):-ae_relation(R,A,A), !.
+is_symmetric(R,A,B):-ae_redundant(R,B,A), ae_redundant(R,A,B),  A \= B, !.
+class_missing(A):- \+ class(A),!.
+
+error((missing_class, A)):-ae_relation(_,_,A), class_missing(A).
+error((missing_class, A)):-ae_relation(_,A,_), class_missing(A).
+error((reflexive,R,A)):-R \= is_a, is_reflexive(R,A).
+error((symmetric, R ,A,B)):-is_symmetric(R, A, B).
+
+ancestor(P, Q):-ae(is_a, P, Q).
+
+common_desendant(C, A, B) :- 
 	ancestor(C, A), 
 	ancestor(C, B).
 
+	
 overlap(A, B, X):-
-	common_desendent(X, A, B).
+	common_desendant(X, A, B).
+	
+overlap_lineally_unrelated(A, B, X):-
+	overlap(A, B, X),
+	\+ ancestor(A, B),
+	\+ ancestor(B, A).
+	
 	
 disjoint(C, D) :- 
 	class(C), 
