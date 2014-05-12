a6565016307660b7a5ed028580d2e170edf52d75
jannick <jannickboese@msn.com>
2014-05-12 23:03:56 +0200

    puny attempts at fixing prolog code

--- relations.pl
+++ relations.pl
@@ -1,8 +1,15 @@
 ['~/Ontological/relationsdb'].
-
+class(a).
+class(b).
+class(c).
+class(f).
+ae_relation(is_a, a, f).
+ae_relation(is_a, a, b).
+ae_relation(is_a, b, c).
+ae_relation(is_a, c, a).
 
 transitivity(R,A,B):-ae_relation(R,A,B).
-transitivity(R,A,C):-ae_relation(is_a,B,C),transitivity(R,A,B).
+transitivity(R,A,C):-R \= is_a,ae_relation(is_a,B,C),transitivity(R,A,B).
 transitivity(R,A,C):-R \= is_a,ae_relation(is_a,A,B),transitivity(R,B,C).
 transitivity(R,A,C):-ae_relation(R,A,B),transitivity(R,B,C).
 
@@ -16,13 +23,12 @@ ae_redundant(is_a, A, A):-class(A).
 ae(R, A, B) :- setof(_, ae_redundant(R,A,B), _).
 
 is_reflexive(R,A):-ae_relation(R,A,A), !.
-is_symmetric(R,A,B):-ae_redundant(R,B,A), ae_redundant(R,A,B),  A \= B, !.
-class_missing(A):- \+ class(A),!.
+is_symmetric(R,A,B):- A \= B, ae_redundant(R,B,A), ae_redundant(R,A,B), !.
+class_missing(A):- (ae_relation(_,_,A); ae_relation(_,A,_)), \+ class(A).
 
-error((missing_class, A)):-ae_relation(_,_,A), class_missing(A).
-error((missing_class, A)):-ae_relation(_,A,_), class_missing(A).
-error((reflexive,R,A)):-R \= is_a, is_reflexive(R,A).
-error((symmetric, R ,A,B)):-is_symmetric(R, A, B).
+error((missing_class, A)):-setof(_, class_missing(A), _).
+error((reflexive,R,A)):-is_reflexive(R,A), R \= is_a.
+error((symmetric, R ,A,B)):-class(A), class(B), is_symmetric(R, A, B).
 
 ancestor(P, Q):-ae(is_a, P, Q).
 
