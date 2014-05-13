7e2712aa70cf218435c83556f4bee6d79a7e5bef
Søren Jacobsen <srnjcbsn@gmail.com>
2014-05-13 15:59:24 +0200

    cleaned up the code

--- relations.pl
+++ relations.pl
@@ -1,17 +1,9 @@
 ['~/Ontological/relationsdb'].
-class(a).
-class(b).
-class(c).
-class(f).
-ae_relation(is_a, a, f).
-ae_relation(is_a, a, b).
-ae_relation(is_a, b, c).
-ae_relation(is_a, c, a).
 
-transitivity(R,A,B):-ae_relation(R,A,B).
-transitivity(R,A,C):-ae_relation(R,A,B),transitivity(R,B,C).
-transitivity(R,A,C):-R \= is_a,ae_relation(is_a,B,C),transitivity(R,A,B).
-transitivity(R,A,C):-R \= is_a,ae_relation(is_a,A,B),transitivity(R,B,C).
+transitivity(R, A, B) :- ae_relation(R,A,B).
+transitivity(R, A, C) :- ae_relation(R,A,B),transitivity(R,B,C).
+transitivity(R, A, C) :- R \= is_a, ae_relation(is_a,B,C), transitivity(R,A,B).
+transitivity(R, A, C) :- R \= is_a, ae_relation(is_a,A,B), transitivity(R,B,C).
 
 ae_redundant(R, A, B) :- transitivity(R,A,B).
 ae_redundant(part_of, A, B) :- 
@@ -21,29 +13,27 @@ ae_redundant(is_a, A, A):-class(A).
 
 ae(R, A, B) :- setof(_, ae_redundant(R,A,B), _).
 
-is_reflexive(R,A):-ae_relation(R,A,A), !.
-is_symmetric(R,A,B):- A \= B, ae_redundant(R,B,A), ae_redundant(R,A,B), !.
-class_missing(A):- (ae_relation(_,_,A); ae_relation(_,A,_)), \+ class(A).
+is_reflexive(R, A) :- ae_relation(R,A,A), !.
+is_symmetric(R, A, B) :- A \= B, ae_redundant(R,B,A), ae_redundant(R,A,B), !.
+class_missing(A) :- (ae_relation(_,_,A); ae_relation(_,A,_)), \+ class(A).
 
-error((missing_class, A)):-setof(_, class_missing(A), _).
-error((reflexive,R,A)):-is_reflexive(R,A), R \= is_a.
-error((symmetric, R ,A,B)):-class(A), class(B), is_symmetric(R, A, B).
+error((missing_class, A)) :- setof(_, class_missing(A), _).
+error((reflexive,R,A)) :- is_reflexive(R,A), R \= is_a.
+error((symmetric, R ,A,B)) :- class(A), class(B), is_symmetric(R, A, B).
 
-ancestor(P, Q):-ae(is_a, P, Q).
+ancestor(P, Q) :- ae(is_a, P, Q).
 
 common_desendant(X, A, B) :- 
 	ancestor(X, A), 
 	ancestor(X, B).
-
 	
-overlap(A, B, X):-
+overlap(A, B, X) :-
 	common_desendant(X, A, B).
 	
-overlap_lineally_unrelated(A, B, X):-
+overlap_lineally_unrelated(A, B, X) :-
 	overlap(A, B, X),
 	\+ ancestor(A, B),
 	\+ ancestor(B, A).
-	
 	
 disjoint(C, D) :- 
 	class(C), 
--- relationsdb.pl
+++ relationsdb.pl
@@ -27,70 +27,51 @@ class(pancreatic_cell).
 class(capillary).
 class(pancreatic_polypeptide).
 
-
 % is_a relationships
-
 ae_relation(is_a, exocrine_cell, cell).
 ae_relation(is_a, endocrine_cell, cell).
 ae_relation(is_a, pancreatic_cell, cell).
 ae_relation(is_a, stem_cell, cell).
 ae_relation(is_a, neuronal_schwann_cell, cell).
-
 ae_relation(is_a, adult_stem_cell, stem_cell).
 ae_relation(is_a, embryonic_stemm_cell, stem_cell).
-
 ae_relation(is_a, exocrine_pancreatic_cell, exocrine_cell).
 ae_relation(is_a, exocrine_pancreatic_cell, pancreatic_cell).
-
 ae_relation(is_a, endocrine_pancreatic_cell, endocrine_cell).
 ae_relation(is_a, endocrine_pancreatic_cell, pancreatic_cell).
-
 ae_relation(is_a, centroacinar_cell, exocrine_pancreatic_cell).
 ae_relation(is_a, acinar_cell, exocrine_pancreatic_cell).
-
 ae_relation(is_a, alpha_cell, endocrine_pancreatic_cell).
 ae_relation(is_a, beta_cell, endocrine_pancreatic_cell).
 ae_relation(is_a, delta_cell, endocrine_pancreatic_cell).
 ae_relation(is_a, pp_cell, endocrine_pancreatic_cell).
-
 ae_relation(secretes, alpha_cell, glucagon).
 ae_relation(secretes, beta_cell, insulin).
 ae_relation(secretes, delta_cell,  gastrin).
 ae_relation(secretes, delta_cell, somatostatin).
 ae_relation(secretes, pp_cell, pancreatic_polypeptide).
 
-
 % has_part relationships
 ae_relation(has_part,nervous_system,neuronal_schwann_cell).
 ae_relation(has_part,pancreas,exocrine_pancreas).
 ae_relation(has_part,pancreas,endocrine_pancreas).
-
 ae_relation(has_part,exocrine_pancreas,exocrine_pancreatic_cell).
 ae_relation(has_part,exocrine_pancreas,duct).
-
 ae_relation(has_part,endocrine_pancreas,islet_of_langerhans).
-
 ae_relation(has_part,islet_of_langerhans,alpha_cell).
 ae_relation(has_part,islet_of_langerhans,beta_cell).
 ae_relation(has_part,islet_of_langerhans,delta_cell).
 ae_relation(has_part,islet_of_langerhans,pp_cell).
 ae_relation(has_part,islet_of_langerhans,capillary).
 
-
 % part_for relationships
-
 ae_relation(part_for,neuronal_schwann_cell,nervous_system).
-
 ae_relation(part_for,exocrine_pancreas,pancreas).
 ae_relation(part_for,endocrine_pancreas,pancreas).
-
 ae_relation(part_for,exocrine_pancreatic_cell,exocrine_pancreas).
 ae_relation(part_for,duct,exocrine_pancreas).
-
 ae_relation(part_for,islet_of_langerhans,endocrine_pancreas).
-
 ae_relation(part_for,alpha_cell,islet_of_langerhans).
 ae_relation(part_for,beta_cell,islet_of_langerhans).
 ae_relation(part_for,delta_cell,islet_of_langerhans).
 ae_relation(part_for,pp_cell,islet_of_langerhans).
-
